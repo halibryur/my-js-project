f21aed85a242b640c1982233839f073da64f0629
halibryur”  qgit config --global user.email “example@Gmail.com <hiyurt7@gmail.com  git config --global user.password passwordqgit config --global user.name halibryur>
2021-08-31 00:43:32 -0700

    first commit

---
+++ Day01_HelloWorld.js
@@ -0,0 +1,7 @@
+// Create a class : Day01_HelloWorld.js
+// How to run js code on IDE?
+// We need Node JS to run javascript outside of the browser
+// node CLASSNAME
+// E.G. node Day01_HelloWorld.js
+console.log('Hello World ! ');
+console.log(10+7);
